19e77045d360fb782b87985a90d1fb452d9641c9
Quin <quin@cheesits456.dev>
2020-09-09 06:42:49 -0700

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require("puppeteer");
-
-(async () => {
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	await page.goto("https://0x.org/zrx/staking");
-	const statsElements = await page.$$eval("#app > main:first-of-type > div:nth-of-type(2) > div:first-of-type > div", stats => stats.map(stat => stat.innerHTML));
-	await browser.close();
-})();
