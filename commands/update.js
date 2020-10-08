4ab0bb0dcb4adceceb89a13811a1400b12fcdc3d
cheesits456 <quin@cheesits456.dev>
2020-10-07 19:27:40 -0700

    Update the update command to work with .env-style configuration

--- commands/update.js
+++ commands/update.js
@@ -17,28 +17,44 @@ class Update extends Command {
 
 	async run(msg, args, data) {
 		const l = msg.client.config.emojis.loading;
-
 		await msg.client.user.setActivity("Updating . . .");
-		let m = await msg.channel.send(`${l} | **Downloading git repo . . .**`);
 
+		// Download git repo
+		let m = await msg.channel.send(`${l} | **Downloading git repo . . .**`);
+		child_process.execSync("git clone https://github.com/cheesits456/0xDiscordBot.git", { cwd: process.env.PWD });
+
+		// Replace old codebase with new stuff
+		await m.edit(`${l} | **Porting config files . . .**`);
 		child_process.execFileSync("./helpers/updater.sh", { cwd: process.env.PWD });
 
-		await m.edit(`${l} | **Porting config files . . .**`);
-
+		// Port values from old config.json into new file
 		delete require.cache[require.resolve("../config")];
-
 		const config = {
 			old: require("../config.bak"),
 			new: require("../config")
 		};
-
 		for (const [key, value] of Object.entries(config.old)) config.new[key] = value;
-
 		await fs.promises.writeFile("./config.json", JSON.stringify(config.new, null, "\t"), "utf8");
 		await fs.promises.unlink("./config.bak.json");
 
-		msg.client.logger.warn(`Restarting ${msg.client.user.username} . . .`);
+		// Create .env file from current environment variables
+		await fs.promises.writeFile("./.env", [
+			"# Discord bot auth token",
+			`DISCORD_TOKEN=${process.env.DISCORD_TOKEN || ""}`,
+			"",
+			"# Twitter API tokens",
+			`TWITTER_CONSUMER_KEY=${process.env.TWITTER_CONSUMER_KEY || ""}`,
+			`TWITTER_CONSUMER_SECRET=${process.env.TWITTER_CONSUMER_SECRET || ""}`,
+			`TWITTER_ACCESS_TOKEN=${process.env.TWITTER_ACCESS_TOKEN || ""}`,
+			`TWITTER_ACCESS_TOKEN_SECRET=${process.env.TWITTER_ACCESS_TOKEN_SECRET || ""}`
+		].join("\n"), "utf8");
 
+		// Update node dependencies
+		await m.edit(`${l} | **Updating dependencies . . .**`);
+		child_process.execSync("npm i", { cwd: process.env.PWD });
+
+		// Restart the bot
+		msg.client.logger.warn(`Restarting ${msg.client.user.username} . . .`);
 		await m.edit(`${l} | **Restarting . . .**`);
 		await fs.promises.writeFile(
 			"./restartMessage.json",
--- helpers/updater.sh
+++ helpers/updater.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-git clone https://github.com/cheesits456/0xDiscordBot.git
-mv config.json config.bak.json
+mv config.json config.bak.json 2> /dev/null
 ls -QA | grep -ve "config.bak.json\|0xDiscordBot\|guilds\|node_modules" | xargs rm -rf
 cp -rf ./0xDiscordBot/. .
 rm -rf ./0xDiscordBot
-npm i
 exit 0
